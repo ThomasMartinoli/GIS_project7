c69faf1aefdb892a98274acc357bf66990614650
ThomasMartinoli <thomas.martinoli@mail.polimi.it>
2021-02-05 16:40:11 +0200

    update website

--- Web/assets/js/map.js
+++ Web/assets/js/map.js
@@ -2,10 +2,39 @@
 var BingBaseMap = new ol.layer.Tile({
 	title: 'Bing Map',
 	type: 'base',
-	visible: true,
+	visible: false,
 	source: new ol.source.BingMaps({
         key:'Ar4e8YGyC3IOzjLgl2bvpIZGA3yIozo4rekktbZBZQklYbRBaGGobPr50fg9z-af',
         imagerySet:'Aerial'
+		})
+
+});
+
+
+var osm = new ol.layer.Tile({
+	title: 'OpenStreetMap',
+	type: 'base',
+	visible: true,
+	source: new ol.source.OSM()
+});
+
+
+var stamenWatercolor = new ol.layer.Tile({
+	title: 'Stamen Watercolor',
+	type: 'base',
+	visible: false,
+	source: new ol.source.Stamen({
+        layer:'watercolor'
+		})
+
+});
+
+var stamenToner = new ol.layer.Tile({
+	title: 'Stamen Toner',
+	type: 'base',
+	visible: false,
+	source: new ol.source.Stamen({
+        layer:'toner'
 		})
 
 });
@@ -35,6 +64,22 @@ var WorldPopS1 = new ol.layer.Image({
 	}),
 	opacity: 0.7,
 });
+
+
+
+var homepage_map = new ol.layer.Image({
+	title:'homepage_map',
+	visible: true,
+	source:new ol.source.ImageWMS({
+		url:'http://localhost:8082/geoserver/GIS_project7/wms',
+		params: {'LAYERS':'GIS_project7:homepage'},
+		ratio: 1,
+		serverType: 'geoserver',
+	}),
+	opacity: 0.7,
+});
+
+
 
 var Intercomp = new ol.layer.Image({
 	title:'Intercomparison',
@@ -85,14 +130,55 @@ var Difference = new ol.layer.Image({
 });
 
 
+/*Create the map homepage*/
+var map_home = new  ol.Map({
+	target: document.getElementById('map_home'),
+	layers: [
+	new ol.layer.Group({
+		title: 'Base Maps',
+		layers: [osm,stamenToner,stamenWatercolor]
+	}),
+	new ol.layer.Group({
+		title: 'Overlay Layer',
+		layers: [homepage_map]
+	}),
+	],
+	view: new ol.View({
+		center: [1100000,5000000],
+		zoom: 1
+	}),
+	controls: ol.control.defaults().extend([
+		new ol.control.ScaleLine(),
+		new ol.control.FullScreen(), 
+		new ol.control.OverviewMap({
+			layers:[
+			new ol.layer.Tile({
+				source: new ol.source.OSM()
+			})
+		]
+		}),
+		new ol.control.MousePosition({
+			coordinateFormat:ol.coordinate.createStringXY(4),
+			projection: 'EPSG:4326'
+		})
+	])
+});
+
+var layerSwitcher = new ol.control.LayerSwitcher({});
+map_home.addControl(layerSwitcher);
+
+
+
+
 /*Create the map*/
 var map = new  ol.Map({
 	target: document.getElementById('map'),
 	layers: [	
 	new ol.layer.Group({
 		title: 'Base Maps',
-		layers: [BingBaseMap]
+		layers: [BingBaseMap,osm,stamenWatercolor]
 	}),
+
 	new ol.layer.Group({
 		title: 'Validation',
 		layers: [ReclassifiedGHSPop, ReclassifiedWorldPop,Difference]
@@ -101,12 +187,18 @@ var map = new  ol.Map({
 		title: 'Intercomparison',
 		layers: [GHSPOPS1, WorldPopS1,Intercomp]
 	}),
+
+	new ol.layer.Group({
+		title: 'Layer',
+		layers: [homepage_map]
+		
+	}),
 	
     ],
 
 	view: new ol.View({
-		center: [13000000,15000000],
-		zoom: 2.5
+		center: [12500000,11900000],
+		zoom: 3.2
 	}),
 	controls: ol.control.defaults().extend([
 		new ol.control.ScaleLine(),
